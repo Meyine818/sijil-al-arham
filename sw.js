93bf94c236105b3620e22805c7f3668a45bab61e
Meyine818 <mariemseyid818@gmail.com>
2026-03-13 04:34:53 +0000

    Simplify service worker caching implementation
    Refactor service worker to simplify caching logic and remove specific cache names.

--- sw.js
+++ sw.js
@@ -1,37 +1,15 @@
-const اسم_الكاش = 'سجل-الأرحام-v3';
-
-const الملفات = [
-    'index.html',
-    'سجل.html',
-    'style.css',
-    'script.js',
-    'manifest.json'
-];
-
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(اسم_الكاش).then(cache => {
-            return cache.addAll(الملفات);
-        })
-    );
     self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== اسم_الكاش)
-                    .map(key => caches.delete(key))
-            );
+            return Promise.all(keys.map(key => caches.delete(key)));
         })
     );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+    event.respondWith(fetch(event.request));
 });
